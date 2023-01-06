7c5f1cf0ba51c8e509b6754548879458a2aa863e
mxkss_btw <maks.20050817@gmail.com>
2023-01-07 00:00:30 +0100

    added breaking graph and changing colors

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-const $board = document.querySelector('.board')
-const ctx = $board.getContext('2d')
-// let points = [
-//     {x: 4, y: 20},
-//     {x: 3, y: 9},
-//     {x: 2, y: 4},
-//     {x: 1, y: 1},
-//     {x: 0, y: 0},
-// ]
-let points = []
-let buffer = prompt('Enter pair of numbers [x,y], example: 15,3')
-while(buffer){
-    if(/\d,\d/.test(buffer)){
-        buffer = buffer.split(',')
-        points.push({x: buffer[0], y: buffer[1]})
-        buffer = prompt('Enter pair of numbers: x,y')
-    }
-    else {
-        alert('Invalid format')
-        buffer = prompt('Enter pair of numbers: x,y')
-    }
+const ctx = document.querySelector('.board').getContext('2d')
+let points = [[]]
+let options = {
+    currentArray: 0,
+    funcColors: ['#ff0000'],
 }
+
+function addPoint() {
+    let buffer = prompt('Enter pair of numbers [x,y], example: 15,3')
+    if(buffer) {
+        buffer = buffer.split(',')
+        if (/\d/.test(buffer[0]) && /\d/.test(buffer[1])) {
+            points[options.currentArray].push({x: buffer[0], y: buffer[1]})
+            displayPoints(options.currentArray)
+        } else {
+            alert('Invalid format')
+            buffer = prompt('Enter pair of numbers: x,y')
+        }
+    }
+    else{}
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'n') {
+        addPoint()
+    } else if(e.key === 'b') {
+        if(points[options.currentArray].length !== 0){
+            options.currentArray++
+            points.push([])
+            options.funcColors.push('#ff0000')
+            addPoint()
+        }
+        else {
+            alert('Add at least 1 point by pressing [N]')
+        }
+    } else if(e.key === 'c') {
+        changeFuncColor()
+    }
+})
 
 function coordinateSystem() {
     // guide lines
@@ -53,15 +70,14 @@ function coordinateSystem() {
     ctx.fillText('X', 480, 270)
     ctx.fillText('Y', 260, 20)
 }
-
 coordinateSystem()
 
-function displayPoints() {
-    ctx.strokeStyle = '#ad0000'
-    ctx.fillStyle = '#ad0000'
+function displayPoints(forArray) {
+    ctx.strokeStyle = options.funcColors[forArray]
+    ctx.fillStyle = options.funcColors[forArray]
     ctx.lineWidth = '2'
     ctx.beginPath()
-    points.forEach((item, i) => {
+    points[forArray].forEach((item, i) => {
         ctx.lineTo(250 + item.x * 10, 250 - item.y * 10)
         ctx.moveTo(252 + item.x * 10, 250 - item.y * 10)
         ctx.arc(250 + item.x * 10, 250 - item.y * 10, 2, 0, 2 * Math.PI)
@@ -70,5 +86,24 @@ function displayPoints() {
     })
     ctx.stroke()
 }
+displayPoints(0)
 
-displayPoints()
+function changeFuncColor() {
+    let func = prompt('Enter number of function which you want to change, example: 2')
+    if(func){
+        if(func > 0 && func <= options.currentArray + 1){
+            let color = prompt('Enter color of function in the hexadecimal form, example: #32A852')
+            if(/^#[0-9a-f]{6}$/i.test(color)){
+                options.funcColors[func - 1] = color.toLowerCase()
+                displayPoints(func - 1)
+            }
+            else {
+                alert('Invalid color')
+            }
+        }
+        else {
+            alert('Invalid number')
+        }
+    }
+    else {}
+}
